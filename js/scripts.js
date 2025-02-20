5c86007a1f1ea7dc35b165c558353e96ffe14c33
DsouzaVernon <48061715+DsouzaVernon@users.noreply.github.com>
2025-02-20 22:52:40 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -219,7 +219,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbwXfZT7r_wLnF2XPIoOqfGuAUsBBiXMn0g664MDnnuip6YvJJ65ePLeQsretSmGvKFEZQ/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbxP7JiwzUemVP1bZXdJiue8cZNX6jNRo9cF_Fko7C91pWEt9_ojOouLiOHit88iYDetgw/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
